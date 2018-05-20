76cac58db1bf3f5ec16808f404e705493b900933
David <david.malmstrm@gmail.com>
2018-05-20 18:43:27 +0200

    tidy up code

--- auto-encoder/auto-encoder.py
+++ auto-encoder/auto-encoder.py
@@ -3,41 +3,53 @@ import numpy as np
 
 # Load data
 
-dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch0.npy'), [1, 0, 2])
-#for i in range(1, 10):
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch1.npy'), [1, 0, 2])
+dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch0.npy'),
+                       [1, 0, 2])
+# for i in range(1, 10):
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch1.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch2.npy'), [1, 0, 2])
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch2.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch4.npy'), [1, 0, 2])
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch4.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch5.npy'), [1, 0, 2])
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch5.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch6.npy'), [1, 0, 2])
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch6.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch7.npy'), [1, 0, 2])
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch7.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch11.npy'), [1, 0, 2])
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch11.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch12.npy'), [1, 0, 2])
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch12.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
-temp_dataset = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch13.npy'), [1, 0, 2])
+temp_dataset = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch13.npy'), [1, 0, 2])
 dataset = np.concatenate([dataset, temp_dataset])
 
-validation_set = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch15.npy'), [1, 0, 2])
+validation_set = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/French/batch15.npy'), [1, 0, 2])
 validation_set = validation_set[0:125, :, :]
-validation_set2 = np.transpose(np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch15.npy'), [1, 0, 2])
+validation_set2 = np.transpose(
+    np.load('/media/karl/Elements/DeepLearningProject/VoxForge/speechT-output/English/batch15.npy'), [1, 0, 2])
 validation_set = np.concatenate([validation_set, validation_set2[0:125, :, :]])
 
 for i in range(dataset.shape[2]):
-    mean = np.mean(dataset[:,:,i])
-    std = np.std(dataset[:,:,i])
-    dataset[:,:,i] = (dataset[:,:,i] - mean) / std
+    mean = np.mean(dataset[:, :, i])
+    std = np.std(dataset[:, :, i])
+    dataset[:, :, i] = (dataset[:, :, i] - mean) / std
 
 for i in range(validation_set.shape[2]):
-        mean = np.mean(validation_set[:,:,i])
-        std = np.std(validation_set[:,:,i])
-        validation_set[:,:,i] = (validation_set[:,:,i] - mean) / std
+    mean = np.mean(validation_set[:, :, i])
+    std = np.std(validation_set[:, :, i])
+    validation_set[:, :, i] = (validation_set[:, :, i] - mean) / std
 
 print(dataset.shape)
 
@@ -47,39 +59,44 @@ dataset_std = np.std(dataset)
 print(dataset_mean)
 print(dataset_std)
 
-#dataset -= dataset_mean
-#dataset /= dataset_std
+# dataset -= dataset_mean
+# dataset /= dataset_std
 
 print(np.mean(validation_set))
 print(np.std(validation_set))
 
-#validation_set -= dataset_mean
-#validation_set /= dataset_std
+
+# validation_set -= dataset_mean
+# validation_set /= dataset_std
 
 def split_indicies(index):
     return index // dataset.shape[1], index % dataset.shape[1]
 
+
 current_index = 0
-indicies = np.random.permutation(dataset.shape[0]*dataset.shape[1])
+indicies = np.random.permutation(dataset.shape[0] * dataset.shape[1])
+
 
 def get_next_batch(batch_size):
     global current_index, indicies
     if current_index + batch_size > len(indicies):
         current_index = 0
-        indicies = np.random.permutation(dataset.shape[0]*dataset.shape[1])
+        indicies = np.random.permutation(dataset.shape[0] * dataset.shape[1])
 
     batch = np.zeros([batch_size, dataset.shape[2]])
     for i in range(batch_size):
         sample_index, frame_index = split_indicies(indicies[current_index + i])
-        batch[i,:] = dataset[sample_index, frame_index, :]
+        batch[i, :] = dataset[sample_index, frame_index, :]
     current_index += batch_size
     return batch
 
+
 def format_validation_set(validation_set):
-    new_val_set = validation_set[0,:,:]
+    new_val_set = validation_set[0, :, :]
     for i in range(1, validation_set.shape[0]):
-        new_val_set = np.concatenate([new_val_set, validation_set[i,:,:]])
+        new_val_set = np.concatenate([new_val_set, validation_set[i, :, :]])
     return new_val_set
+
 
 validation_set = format_validation_set(validation_set)
 print(validation_set.shape)
@@ -95,13 +112,14 @@ batch_size = 250
 display_step = 1000
 
 # Network Parameters
-num_input = 250 # Input size
-num_hidden_1 = 1000 # 1st layer num features
-num_hidden_2 = 1000 # 2st layer num features
-num_hidden_3 = 50 # latent dimension
+num_input = 250  # Input size
+num_hidden_1 = 1000  # 1st layer num features
+num_hidden_2 = 1000  # 2st layer num features
+num_hidden_3 = 50  # latent dimension
 
 # tf Graph input (only pictures)
 X = tf.placeholder("float", [None, num_input])
+
 
 def build_encoder(inputs):
     encoder1 = tf.layers.dense(inputs=inputs,
@@ -124,6 +142,7 @@ def build_encoder(inputs):
 
     return encoder3
 
+
 def build_decoder(inputs):
     decoder1 = tf.layers.dense(inputs=inputs,
                                units=num_hidden_2,
@@ -144,9 +163,9 @@ def build_decoder(inputs):
                                use_bias=False)
     return decoder3
 
+
 encoder = build_encoder(X)
 decoder = build_decoder(encoder)
-
 
 # Prediction
 y_pred = decoder
@@ -168,18 +187,17 @@ weights = None
 # Start Training
 # Start a new TF session
 with tf.Session() as sess:
-
     # Run the initializer
     sess.run(init)
     current_index = 0
-    indicies = np.random.permutation(dataset.shape[0]*dataset.shape[1])
+    indicies = np.random.permutation(dataset.shape[0] * dataset.shape[1])
 
     validation_loss = sess.run([loss], feed_dict={X: validation_set})
     print("Initial validation loss: " + str(np.sqrt(validation_loss)))
     best_known_loss = np.sqrt(validation_loss)
 
     # Training
-    for i in range(1, num_steps+1):
+    for i in range(1, num_steps + 1):
         # Prepare Data
         # Get the next batch of MNIST data (only images are needed, not labels)
         batch_x = get_next_batch(batch_size)
@@ -197,7 +215,8 @@ with tf.Session() as sess:
             if np.sqrt(validation_loss) < best_known_loss:
                 best_known_loss = np.sqrt(validation_loss)
                 model_name = "auto-enc-" + str(i) + "-" + ("{0:.4f}".format(np.sqrt(validation_loss))).split('.')[1]
-                save_path = saver.save(sess, "/media/karl/Elements/DeepLearningProject/auto-encoder/" + model_name + "/auto-enc.ckpt")
+                save_path = saver.save(sess,
+                                       "/media/karl/Elements/DeepLearningProject/auto-encoder/" + model_name + "/auto-enc.ckpt")
                 print("Model saved in path: %s" % save_path)
 
     validation_loss = sess.run([loss], feed_dict={X: validation_set})
