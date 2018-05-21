3f09246ad77c342e1f0fb01555f19ba437dcf222
Karl <kalle@kvarnfors.com>
2018-05-21 20:14:35 +0200

    Add construction of validation set and validation loss and accuracy evaluation

--- dnn/dnn.py
+++ dnn/dnn.py
@@ -12,23 +12,23 @@ import numpy as np
 import os
 import random
 
-minibatch_size = 100
-frame_cutoff = 100
+frame_cutoff = 100 # How many frames to use per sample
 
 # Parameters
 learning_rate = 0.1
-num_steps = 500
-batch_size = 128
+num_steps = 1000
+minibatch_size = 128
 display_step = 100
+num_validation_set_minibatches = 10
 
 # Network Parameters
 n_hidden_1 = 256  # 1st layer number of neurons
 n_hidden_2 = 256  # 2nd layer number of neurons
-num_input = 5000  # MNIST data input (img shape: 28*28)
-num_classes = 2  # MNIST total classes (0-9 digits)
+num_input = frame_cutoff * 50  # frame_cutoff frames * 50 elments per frame
+num_classes = 2  # English or French
 
-training_data_folder_path = "/media/david/Elements/David/deep/final_data/labelled/test/"
-validation_data_folder_path = "/media/david/Elements/David/deep/final_data/labelled/validation/"
+training_data_folder_path = "/media/karl/Elements/DeepLearningProject/VoxForge/dataset/labelled/training/"
+validation_data_folder_path = "/media/karl/Elements/DeepLearningProject/VoxForge/dataset/labelled/validation/"
 
 
 def load_data(input_path):
@@ -42,8 +42,7 @@ def load_data(input_path):
             data = np.concatenate([data, data_batch])
     return data
 
-
-def next_mini_batch(data_list):
+def next_mini_batch(data_list, epoch_callback=None):
     while True:
         for i in range(len(data_list)//minibatch_size):
             minibatch = data_list[i * minibatch_size:(i + 1) * minibatch_size]
@@ -66,7 +65,8 @@ def next_mini_batch(data_list):
                     batch_y = np.concatenate([batch_y, sample_y])
 
             yield batch_x, batch_y
-        print("looped")
+        if epoch_callback is not None:
+            epoch_callback()
 
 
 training_data = load_data(training_data_folder_path)
@@ -116,34 +116,42 @@ accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 # Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
 
+# Build validation set
+
+validation_set_x = None
+validation_set_y = None
+
+validation_generator = next_mini_batch(validation_data)
+for i in range(num_validation_set_minibatches):
+    batch_x, batch_y = next(validation_generator)
+    if validation_set_x is not None:
+        validation_set_x = np.concatenate([validation_set_x, batch_x])
+        validation_set_y = np.concatenate([validation_set_y, batch_y])
+    else:
+        validation_set_x = batch_x
+        validation_set_y = batch_y
+import pdb; pdb.set_trace()
 # Start training
 training_generator = next_mini_batch(training_data)
-validation_generator = next_mini_batch(validation_data)  # TODO: HIGH PRIO: this does not good for the validation data, fix
 with tf.Session() as sess:
 
     # Run the initializer
     sess.run(init)
 
     for step in range(1, num_steps+1):
-        batch_x, batch_y = next(training_data)
+        batch_x, batch_y = next(training_generator)
         # Run optimization op (backprop)
         sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
         if step % display_step == 0 or step == 1:
             # Calculate batch loss and accuracy
             loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
                                                                  Y: batch_y})
-            #batch_x_val, batch_y_val = next(validation_data)
-            #loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
-                                                                 #Y: batch_y})
+
+            val_loss, val_acc = sess.run([loss_op, accuracy], feed_dict={X: validation_set_x,
+                                                                         Y: validation_set_y})
             print("Step " + str(step) + ", Minibatch Loss= " + \
                   "{:.4f}".format(loss) + ", Training Accuracy= " + \
-                  "{:.3f}".format(acc))
-
+                  "{:.3f}".format(acc) + ", Validation Loss= " + \
+                  "{:.4f}".format(val_loss) + ", Validation Accuracy= " + \
+                  "{:.3f}".format(val_acc))
     print("Optimization Finished!")
-
-"""
-    # Calculate accuracy for MNIST test images
-    print("Testing Accuracy:", \
-        sess.run(accuracy, feed_dict={X: mnist.test.images,
-Y: mnist.test.labels}))
-"""
